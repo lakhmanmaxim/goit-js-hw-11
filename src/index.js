efb9d4135544ad38f47471a954210d01b5ca33a6
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-12-18 12:13:59 +0200

    build setup
    build setup

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import { fetchCountries } from './fetch-countries.js';
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+
+// const inputCountry = document.querySelector('#search-box');
+// const countryList = document.querySelector('.country-list');
+// const countryInfo = document.querySelector('.country-info');
+
+// inputCountry.addEventListener(
+//   'input',
+//   debounce(onInputEnteredValue, DEBOUNCE_DELAY)
+// );
+
+// function onInputEnteredValue(evt) {
+//   const inputValue = evt.target.value.trim();
+//   // console.log(inputValue);
+
+//   if (inputValue === '') {
+//     clearOutput();
+//     // Notiflix.Report.warning(
+//     //   'Your request is EMPTY',
+//     //   'Please enter a more specific name.',
+//     //   'OK'
+//     // );
+//     return;
+//   }
+
+//   fetchCountries(inputValue).then(createMarkup).catch(catchError);
+// }
+
+// function createMarkup(countries) {
+//   clearOutput();
+
+//   if (countries.length > 10) {
+//     // Notiflix.Report.warning(
+//     //   'Too many matches found...',
+//     //   'Too many matches found. Please enter a more specific name.',
+//     //   'OK'
+//     // );
+//     Notiflix.Notify.failure(
+//       'Too many matches found. Please enter a more specific name.'
+//     );
+//     // return;
+//   } else if (countries.length === 1) {
+//     clearOutput();
+//     let lang = '';
+//     const country = countries
+//       .map(({ name, flags, capital, population, languages }) => {
+//         for (let value in languages) {
+//           lang = languages[value];
+//         }
+
+//         return `
+//     <ul>
+//       <li class="country-info_list"><img class="country-info_img" src="${flags.svg}" alt='Country flag' width='40' height ='25'><p class="country-info_country-text">${name.official}</p></li>
+//       <li class="country-info_list">Capital: <p class="country-info_text">${capital}</p></li>
+//       <li class="country-info_list">Population: <p class="country-info_text">${population}</p></li>
+//       <li class="country-info_list">Languages: <p class="country-info_text">${lang}</p></li>
+//     </ul>
+//     `;
+//       })
+//       .join('');
+//     countryInfo.insertAdjacentHTML('beforeend', country);
+//   } else {
+//     clearOutput();
+
+//     const markup = countries
+//       .map(({ name, flags }) => {
+//         // console.log(name, flags);
+//         return `
+//       <li><img src="${flags.svg}" alt="flag" width='20' height ='15'> ${name.official}</li>
+//       `;
+//       })
+//       .join('');
+//     countryList.insertAdjacentHTML('beforeend', markup);
+//   }
+// }
+
+// function clearOutput() {
+//   countryList.innerHTML = '';
+//   countryInfo.innerHTML = '';
+// }
+
+// function catchError(error) {
+//   Notiflix.Report.warning(
+//     'WRONG REQUEST',
+//     'Oops, there is no country with that name',
+//     'Enter new request'
+//   );
+// }
