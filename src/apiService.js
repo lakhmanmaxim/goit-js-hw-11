4719f89a8c8822467217dfb46e6bbb9752340d3e
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-12-24 14:36:36 +0200

    фетч и load more работают
    Сделан КЛАСС.
    фетч и load more работают.
    Нужно работать с разметкой

--- src/apiService.js
+++ src/apiService.js
@@ -1,13 +1,23 @@
 export class ApiService {
-  constructor() {}
+  constructor() {
+    this.searchQuery = '';
+  }
 
-  fetchRequest(inputValue) {
+  fetchRequest() {
     const URL = 'https://pixabay.com/api/';
 
     const API_KEY = '32163007-39541f17b63243672bcfecf36';
 
     fetch(
-      `${URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
+      `${URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`
     ).then(response => response.json());
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const DEBOUNCE_DELAY = 300;
 
 const searchForm = document.querySelector('#search-form');
 const inputSearch = document.querySelector('[name="searchQuery"]');
+const loadMore = document.querySelector('.btn');
 
 const ApiRequest = new ApiService();
 
@@ -15,34 +16,57 @@ inputSearch.addEventListener(
 );
 
 searchForm.addEventListener('submit', onFormSubmit);
+loadMore.addEventListener('click', onLoadMore);
 
 function onInputEnteredValue(evt) {
   const inputValue = evt.target.value;
-  // console.log(inputValue);
+  // // console.log(inputValue);
 
-  if (inputValue === '') {
-    clearOutput();
-    Notiflix.Report.warning(
-      'Your request is EMPTY',
-      'Please enter a more specific request.',
-      'OK'
-    );
-    return;
-  }
-  clearOutput();
+  // if (inputValue === '') {
+  //   clearOutput();
+  //   Notiflix.Report.warning(
+  //     'Your request is EMPTY',
+  //     'Please enter a more specific request.',
+  //     'OK'
+  //   );
+  //   return;
+  // }
+  // clearOutput();
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  let inputValue = evt.currentTarget.elements.searchQuery.value;
-  console.log(inputValue);
+  ApiRequest.query = evt.currentTarget.elements.searchQuery.value.trim();
 
-  ApiRequest.fetchRequest(inputValue); //.then(console.log); //then(createMarkup).catch(catchError);
+  if (ApiRequest.query === '') {
+    Notiflix.Report.warning(
+      'Your request is EMPTY',
+      'Please enter a more specific request.',
+      'OK'
+    );
+    clearOutput();
+    return;
+  }
+
+  ApiRequest.fetchRequest(); //then(createMarkup).catch(catchError);
+}
+
+function onLoadMore() {
+  if (ApiRequest.query === '') {
+    Notiflix.Report.warning(
+      'Your request is EMPTY',
+      'Please enter a more specific request.',
+      'OK'
+    );
+    clearOutput();
+    return;
+  }
+  ApiRequest.fetchRequest();
 }
 
 function clearOutput() {
-  inputValue = '';
+  ApiRequest.query = '';
 }
 
 function catchError(error) {
